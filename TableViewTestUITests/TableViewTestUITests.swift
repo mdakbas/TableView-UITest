df6aab72e2a88a3a794c3b6475405af3d257388c
= <=>
2023-09-04 17:23:01 +0300

    UITest

--- TableViewTestUITests/TableViewTestUITests.swift
+++ TableViewTestUITests/TableViewTestUITests.swift
@@ -8,34 +8,44 @@
 import XCTest
 
 final class TableViewTestUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func testaddNoiseMaker() {
+        
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let newNameButton = app.navigationBars["TableViewTest.HomeVC"].buttons["Add"]
+        
+    
+        let nameField = app.alerts["Add Noise Maker"].scrollViews.otherElements.collectionViews/*@START_MENU_TOKEN@*/.textFields["Shaggy"]/*[[".cells.textFields[\"Shaggy\"]",".textFields[\"Shaggy\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        
+        let addButton = app.alerts["Add Noise Maker"].scrollViews.otherElements.buttons["Add"]
+        let addedCell = app.tables.staticTexts["Mert"]
+       
+        newNameButton.tap()
+        nameField.tap()
+        nameField.typeText("Mert")
+        addButton.tap()
+        XCTAssertTrue(addedCell.exists)
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testDeleteAddNoiseMaker() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        let addButton = app.navigationBars["TableViewTest.HomeVC"].buttons["Add"]
+        let nameTextField = app.alerts["Add Noise Maker"].scrollViews.otherElements.collectionViews/*@START_MENU_TOKEN@*/.textFields["Shaggy"]/*[[".cells.textFields[\"Shaggy\"]",".textFields[\"Shaggy\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+       let addedCell = app.alerts["Add Noise Maker"].scrollViews.otherElements.buttons["Add"]
+        let tablesQuery = app.tables
+ 
+        let deleteAction = tablesQuery/*@START_MENU_TOKEN@*/.buttons["Delete"]/*[[".cells.buttons[\"Delete\"]",".buttons[\"Delete\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        
+        addButton.tap()
+        nameTextField.tap()
+        nameTextField.typeText("Oyku")
+        addedCell.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Oyku"]/*[[".cells.staticTexts[\"Oyku\"]",".staticTexts[\"Oyku\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeLeft()
+        deleteAction.tap()
+        XCTAssertFalse(addedCell.exists)
+        
     }
 }
